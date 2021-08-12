c8b3f166df3c61aff99c6b9c8b141a562c1aa7e8
Gustavo Gomes <gustavo.gomes@fornecedores.sicoob.com.br>
2021-08-12 16:47:03 -0300

    Adicionado lombok para processar antes do mapperStruct

--- src/main/java/com/dryve/dryvecarros/mapper/VeiculoMapper.java
+++ src/main/java/com/dryve/dryvecarros/mapper/VeiculoMapper.java
@@ -3,9 +3,15 @@ package com.dryve.dryvecarros.mapper;
 import com.dryve.dryvecarros.dto.VeiculoDTO;
 import com.dryve.dryvecarros.modelo.Veiculo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface VeiculoMapper {
 
+    @Mappings({
+            @Mapping(target="modelo.id", source="dto.idModelo")
+
+    })
     Veiculo toEntity(VeiculoDTO dto);
 }
