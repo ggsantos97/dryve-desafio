5aa81f632012bc02523a1c1152715c904d0c5984
Gustavo <guga.tavoo@gmail.com>
2021-08-14 02:15:38 -0300

    Adicionado Exchange com Producer rabbitMQ, Projeto Funcional

--- src/main/java/com/dryve/dryvecarros/service/VeiculoService.java
+++ src/main/java/com/dryve/dryvecarros/service/VeiculoService.java
@@ -1,6 +1,17 @@
 package com.dryve.dryvecarros.service;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.dryve.dryvecarros.adapter.FipeIntegracaoRest;
+import com.dryve.dryvecarros.config.RabbitMQConfig;
 import com.dryve.dryvecarros.dto.VeiculoDTO;
 import com.dryve.dryvecarros.dto.VeiculoResponseDTO;
 import com.dryve.dryvecarros.exception.EnumMensagensErro;
@@ -8,13 +19,7 @@ import com.dryve.dryvecarros.exception.ErroNegocialException;
 import com.dryve.dryvecarros.mapper.VeiculoMapper;
 import com.dryve.dryvecarros.modelo.Veiculo;
 import com.dryve.dryvecarros.repository.VeiculoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.Optional;
+import com.google.gson.Gson;
 
 @Service
 public class VeiculoService implements IVeiculoService{
@@ -28,6 +33,9 @@ public class VeiculoService implements IVeiculoService{
     
     @Autowired
     private FipeIntegracaoRest fipeIntegracaoRest;
+    
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @Override
     public VeiculoResponseDTO salva(VeiculoDTO dto) throws ErroNegocialException {
@@ -36,9 +44,19 @@ public class VeiculoService implements IVeiculoService{
         entity.setDataCadastro(LocalDate.now());
         entity.setPrecoFipe(fipeIntegracaoRest.consultaPrecoFipe(String.valueOf(dto.getIdarca()), dto.getIdModelo(), dto.getAno()));
         entity.setModelo(modeloService.buscaModeloPorFipeId(dto.getIdModelo()));
-           return mapper.toResponseDTO(veiculoRepository.save(entity)) ;
+        VeiculoResponseDTO retorno = mapper.toResponseDTO(veiculoRepository.save(entity));
+        enviaMensagemRabbitMQ(retorno);
+           return retorno ;
     }
     
+    private void enviaMensagemRabbitMQ(VeiculoResponseDTO dto) {
+    	try {
+    		 Gson gson = new Gson();
+			rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME,"", gson.toJson(dto));
+		} catch (AmqpException e) {
+			e.printStackTrace();
+		}
+    }
 
     public void validaVeiculoNaBase(String placa) throws ErroNegocialException {
         Optional<Veiculo> veiculoOptional = veiculoRepository.findByPlaca(placa);
